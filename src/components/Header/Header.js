ddf3ec22db50095902d55f2d70e10edaf8b04d05
Vu Duc Hai <hai13032001@gmail.com>
2023-05-17 16:50:50 +0700

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import TopHeader from "./TopHeader";
-import BottomHeader from "./BottomHeader";
 
 function Header() {
-    return(
-        <div className="main--header">
-            <TopHeader></TopHeader>
-            
-        </div>
-    );
+  return (
+    <div className="main--header">
+      <TopHeader></TopHeader>
+    </div>
+  );
 }
 
 export default Header;
